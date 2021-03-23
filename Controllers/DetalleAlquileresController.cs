2f49e2c963fe91deef8b5ea3130c343c5a57176b
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2021-03-22 21:13:38 -0500

    SE CREA CONTROLADOR DETALLE DE ALQUIER Y SANCION

---
+++ Controllers/DetalleAlquileresController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend_Alquiler.Models;
+
+namespace Backend_Alquiler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DetalleAlquileresController : ControllerBase
+    {
+        private readonly ContextDB _context;
+
+        public DetalleAlquileresController(ContextDB context)
+        {
+            _context = context;
+        }
+
+        // GET: api/DetalleAlquileres
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DetalleAlquiler>>> GetDetalleAlquileres()
+        {
+            return await _context.DetalleAlquileres.ToListAsync();
+        }
+
+        // GET: api/DetalleAlquileres/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DetalleAlquiler>> GetDetalleAlquiler(int id)
+        {
+            var detalleAlquiler = await _context.DetalleAlquileres.FindAsync(id);
+
+            if (detalleAlquiler == null)
+            {
+                return NotFound();
+            }
+
+            return detalleAlquiler;
+        }
+
+        // PUT: api/DetalleAlquileres/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDetalleAlquiler(int id, DetalleAlquiler detalleAlquiler)
+        {
+            if (id != detalleAlquiler.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(detalleAlquiler).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DetalleAlquilerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/DetalleAlquileres
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<DetalleAlquiler>> PostDetalleAlquiler(DetalleAlquiler detalleAlquiler)
+        {
+            _context.DetalleAlquileres.Add(detalleAlquiler);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDetalleAlquiler", new { id = detalleAlquiler.Id }, detalleAlquiler);
+        }
+
+        // DELETE: api/DetalleAlquileres/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDetalleAlquiler(int id)
+        {
+            var detalleAlquiler = await _context.DetalleAlquileres.FindAsync(id);
+            if (detalleAlquiler == null)
+            {
+                return NotFound();
+            }
+
+            _context.DetalleAlquileres.Remove(detalleAlquiler);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DetalleAlquilerExists(int id)
+        {
+            return _context.DetalleAlquileres.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/SancionesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Backend_Alquiler.Models;
+
+namespace Backend_Alquiler.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SancionesController : ControllerBase
+    {
+        private readonly ContextDB _context;
+
+        public SancionesController(ContextDB context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Sanciones
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Sancion>>> GetSanciones()
+        {
+            return await _context.Sanciones.ToListAsync();
+        }
+
+        // GET: api/Sanciones/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Sancion>> GetSancion(int id)
+        {
+            var sancion = await _context.Sanciones.FindAsync(id);
+
+            if (sancion == null)
+            {
+                return NotFound();
+            }
+
+            return sancion;
+        }
+
+        // PUT: api/Sanciones/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSancion(int id, Sancion sancion)
+        {
+            if (id != sancion.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(sancion).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SancionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Sanciones
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Sancion>> PostSancion(Sancion sancion)
+        {
+            _context.Sanciones.Add(sancion);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSancion", new { id = sancion.Id }, sancion);
+        }
+
+        // DELETE: api/Sanciones/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSancion(int id)
+        {
+            var sancion = await _context.Sanciones.FindAsync(id);
+            if (sancion == null)
+            {
+                return NotFound();
+            }
+
+            _context.Sanciones.Remove(sancion);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SancionExists(int id)
+        {
+            return _context.Sanciones.Any(e => e.Id == id);
+        }
+    }
+}
