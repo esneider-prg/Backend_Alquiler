eeaf07cf1085ac7dc0be03935d1bc882c8bc88e6
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2021-03-22 20:22:28 -0500

    SE CREA CONTROLADOR DE ALQUILERES

--- Models/Alquiler.cs
+++ Models/Alquiler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,13 @@ namespace Backend_Alquiler.Models
     {
         public int Id { get; set; }
 
+        [Range(1, int.MaxValue, ErrorMessage = "El ID del CLIENTE es obligatorio y debe ser un entero valido dentro del rango")]
         public int ClienteId { get; set; }
 
+        [Required(ErrorMessage = "La fecha del alquiler es obligatoria")]
         public string FechaAlquiler { get; set; }
 
+        [Range(1000, int.MaxValue, ErrorMessage = "El valor del alquiler es obligatorio y debe ser un entero valido dentro del rango")]
         public int ValorAlquiler { get; set; }
 
         public Cliente Cliente { get; set; }
